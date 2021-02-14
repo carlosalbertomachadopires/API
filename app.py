242488b40da5fd216b69f45a8d06d363af6e58a4
CPDEZ08 <carlosalbertomachadopires@gmail.comm>
2021-02-14 12:05:36 -0600

    APIcodes

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import requests
+from config import api_key
+from pprint import pprint
+
 
 # url = "https://jsonplaceholder.typicode.com/posts/"
 
@@ -14,17 +17,29 @@ import requests
 
 # print(t)
 
-url = "https://www.omdbapi.com/"
-api_key = "trilogy"
-title = ["Aliens", "Sing", "Moana"]
-title_director = []
-for movie_title in title:
-    data = requests.get(url, params={
-    "apikey": api_key,
-    "t": movie_title}).json()
+# url = "https://www.omdbapi.com/"
+# api_key = "trilogy"
+# title = ["Aliens", "Sing", "Moana"]
+# title_director = []
+# for movie_title in title:
+#     data = requests.get(url, params={
+#     "apikey": api_key,
+#     "t": movie_title}).json()
 
-    title_director.append(data)
-    print(data["Director"])
-print(title_director)
+#     title_director.append(data)
+#     print(data["Director"])
+# print(title_director)
 
 
+url = "https://api.nytimes.com/svc/search/v2/articlesearch.json"
+
+payload = {
+    "api-key": api_key,
+    "q": "entry-level-job-opportunities-2020"
+}
+data = requests.get(url, params=payload).json()
+# print(data)
+data_articles = data["response"]["docs"]
+pprint(data_articles)
+for data in data_articles:
+    pprint(data["web_url"]) 
